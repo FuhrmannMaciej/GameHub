38f1b705f10918d02fee5af5ab7fb45206c30670
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2023-03-24 16:46:36 +0100

    fix: config setup eas

--- app.config.js
+++ app.config.js
@@ -34,7 +34,10 @@ export default {
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID
+      appId: process.env.APP_ID,
+      eas: {
+        projectId: process.env.EAS_PROJECT_ID
+      }
     }
   }
 }
